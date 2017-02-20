8c0e6d8f275d899316f6b8551b69219f72da87b6
austincrisp <austin.crisp05@gmail.com>
2017-02-20 12:09:54 -0500

    Using client WebAPIs

---
+++ Day1APIClient/Models/People.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1APIClient.Models
+{
+    class PeopleCollection
+    {
+        public string Count { get; set; }
+        public string Next { get; set; }
+        public string Previous { get; set; }
+        public List<People> Results { get; set; }
+
+        public PeopleCollection GetPage(HttpClient client, Uri page)
+        {
+            string pageNumber = page.Query;
+            if (pageNumber != null)
+            {
+                var allPeopleResponse = client.GetAsync($"people{pageNumber}").Result;
+                return allPeopleResponse.Content.ReadAsAsync<PeopleCollection>().Result;
+            }
+            return this;
+        }
+
+        public PeopleCollection GetPrevious(HttpClient client)
+        {
+            return GetPage(client, Previous);
+        }
+
+        public PeopleCollection GetNext(HttpClient client)
+        {
+            return GetPage(client, Next);
+        }
+    }
+
+    class People
+    {
+        public string Name { get; set; }
+        public string Height { get; set; }
+        public string Mass { get; set; }
+        public Uri Homeworld { get; set; }
+
+        public Planet HomeworldDetail(HttpClient client)
+        {
+            var response = client.GetAsync(Homeworld).Result;
+            Planet homeworld = response.Content.ReadAsAsync<Planet>().Result;
+            return homeworld;
+        }
+    }
+}
--- Day1APIClient/Program.cs
+++ Day1APIClient/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Day1APIClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,25 @@ namespace Day1APIClient
 {
     class Program
     {
+        private static HttpClient client = new HttpClient();
+
+        private static void SetUpClient()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/JSON"));
+            client.BaseAddress = new Uri("https://swapi.co/api");
+        }
+
         static void Main(string[] args)
         {
+            SetUpClient();
+            var response = client.GetAsync("people/1").Result;
+            //var luke = response.Content.ReadAsStringAsync().Result;
+            People luke = response.Content.ReadAsAsync<People>().Result;
+
+            var allPeopleResponse = client.GetAsync("people").Result;
+            PeopleCollection allPeople = allPeopleResponse.Content.ReadAsAsync<PeopleCollection>.Result;
+            var nextPage = allPeople.GetNext(client);
         }
     }
 }
